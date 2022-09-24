3ca709fdd4f58a42490846077469107dd6e6210a
circusfloor <942408787@qq.com>
2022-09-24 15:27:06 +0800

    git init

--- rw_visual.py
+++ rw_visual.py
@@ -1,12 +1,24 @@
 import matplotlib.pyplot as plt
-
 from randomwalk import RandomWalk
+
 
 while True:
     # 创建一个RandomWalk实例，并将其包含的点都绘制出来
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
-    plt.scatter(rw.x_values, rw.y_values, s=15)
+
+    plt.figure(figsize=(10, 6))
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+
+    # 突出起点和终点
+    plt.scatter(0, 0, c='green', edgecolors='none', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+    # 隐藏坐标轴
+    # plt.axes().get_xaxis().set_visible(False)
+    # plt.axes().get_yaxis().set_visible(False)
+
     plt.show()
 
     keep_running = input('make anther walk?(y/n):')
